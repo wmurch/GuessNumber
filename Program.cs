2952a95e45978bf408a5e5ace9405ae192a2224d
Will Murchison <wmurch@hotmail.com>
2019-06-11 14:33:19 -0400

    initial code

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace GuessNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random randomNum = new Random();
+            int randomTarget = randomNum.Next(100) + 1;
+            int numGuess = 0;
+
+            while (numGuess != randomTarget)
+            {
+                Console.WriteLine("Guess a number between 1 and 100: ");
+                int.TryParse(Console.ReadLine(), out numGuess);
+                if (numGuess > randomTarget)
+                {
+                    Console.WriteLine("{0} is too high!", numGuess);
+                }
+                else if (numGuess < randomTarget)
+                {
+                    Console.WriteLine("{0} is too low!", numGuess);
+                }
+                else
+                {
+                    Console.WriteLine("{0} is right congratulations!", numGuess);
+                }
+            }
+        }
+    }
+}
